f7f92a4e5925f6e0a8490e45221a64bea8c938ba
Nanako Ohno <nanako@wever.co.jp>
2019-10-10 15:05:47 +0900

    responsive added

--- src/components/molecules/card/blog-profile.js
+++ src/components/molecules/card/blog-profile.js
@@ -1,31 +1,62 @@
 import React, { Component } from 'react'
-import { Image, Card, Icon } from 'semantic-ui-react';
+import { Image, Card, Icon, Responsive, Grid, Segment, GridColumn } from 'semantic-ui-react';
+import { Link } from 'gatsby'
 
 export default class BlogProfile extends Component {
     render() {
         return (
-            <Card>
-                <Card.Content textAlign='center'>
-                    <Image
-                        size='small'
-                        src='https://react.semantic-ui.com/images/wireframe/image.png'
-                    />
-                    <Card.Header>大野奈那子　</Card.Header>
-                    <Card.Description>
-                        エンジニア志望の大学３年生
-                    </Card.Description>
-                </Card.Content>
-                <Card.Content>
-                    <p>
-                        <Icon name='instagram' />
-                        Nanako0823
-                    </p>
-                    <p>
-                        <Icon name='github' />
-                        Nana0823
-                    </p>
-                </Card.Content>
-            </Card>
+            <div>
+                <Responsive minWidth={710}>
+                    <Card>
+                        <Card.Content textAlign='center'>
+                            <Image
+                                src='https://react.semantic-ui.com/images/wireframe/image.png'
+                            />
+                            <Link to='profile-page'>
+                                <Card.Header>大野奈那子　</Card.Header>
+                            </Link>
+                            <Card.Description>
+                                エンジニア志望の大学３年生
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content>
+                            <p>
+                                <Icon name='instagram' />
+                                Nanako0823
+                            </p>
+                            <p>
+                                <Icon name='github' />
+                                Nana0823
+                            </p>
+                        </Card.Content>
+                    </Card>
+                </Responsive>
+                <Responsive maxWidth={710}>
+                    <Segment>
+                        <Grid>
+                            <GridColumn width={4}>
+                                <Image src='https://react.semantic-ui.com/images/wireframe/square-image.png' circular />
+                            </GridColumn>
+                            <GridColumn width={12}>
+                                <Link to='profile-page'>
+                                    <h5 style={{ marginBottom: '10px' }}>大野奈那子</h5>
+                                </Link>
+                                エンジニア志望の大学３年生。
+                                <p>
+                                    <span>
+                                        <Icon name='instagram' />
+                                        Nanako0823
+                                </span>
+                                    <span style={{ marginLeft: '5px' }}>
+                                        <Icon name='github' />
+                                        Nana0823
+                                </span>
+                                </p>
+                            </GridColumn>
+                        </Grid>
+                    </Segment>
+                </Responsive>
+            </div>
         )
     }
 }
--- src/components/organisms/blog/index.js
+++ src/components/organisms/blog/index.js
@@ -4,26 +4,38 @@ import BlogItem from './blog'
 import RecommendBlog from './recommend-blog'
 import BlogProfile from '../../molecules/card/blog-profile'
 import BlogPicture from '../../atoms/blog-picture'
-import { Grid, GridColumn, Segment } from 'semantic-ui-react';
+import { Grid, GridColumn, Segment, Responsive } from 'semantic-ui-react';
 
 export default class Blog extends Component {
     render() {
         return (
+
             <div style={{ margin: '0 auto', maxWidth: '960px' }}>
                 <BlogPicture />
-                <Grid>
-                    <GridColumn width={12}>
-                        <BlogItem />
-                        <Segment>
-                            <h3 style={{ textAlign: 'center' }}>オススメの記事</h3>
-                            <RecommendBlog />
-                        </Segment>
-                        <RecentPosts />
-                    </GridColumn>
-                    <GridColumn width={4}>
-                        <BlogProfile />
-                    </GridColumn>
-                </Grid>
+                <Responsive minWidth={710}>
+                    <Grid>
+                        <GridColumn width={12}>
+                            <BlogItem />
+                            <Segment>
+                                <h3 style={{ textAlign: 'center' }}>オススメの記事</h3>
+                                <RecommendBlog />
+                            </Segment>
+                            <RecentPosts />
+                        </GridColumn>
+                        <GridColumn width={4}>
+                            <BlogProfile />
+                        </GridColumn>
+                    </Grid>
+                </Responsive>
+                <Responsive maxWidth={710}>
+                    <BlogItem />
+                    <Segment>
+                        <h3 style={{ textAlign: 'center' }}>オススメの記事</h3>
+                        <RecommendBlog />
+                    </Segment>
+                    <RecentPosts />
+                    <BlogProfile />
+                </Responsive>
             </div>
         )
     }
