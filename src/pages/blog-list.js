2feb38dda3c58536e38bbe177689dc0d81cebb14
Nanako Ohno <nanako@wever.co.jp>
2019-10-12 17:01:56 +0900

    pagination added

--- src/pages/blog-list.js
+++ src/pages/blog-list.js
@@ -3,7 +3,7 @@ import BlogCard from '../components/molecules/card/blog-card'
 import Theme from '../components/organisms/side-menu/theme'
 import BlogHeader from '../components/molecules/header/blog-header'
 import Layout from '../components/layout'
-import { Grid, GridColumn, Responsive } from 'semantic-ui-react';
+import { Grid, GridColumn, Responsive, Pagination } from 'semantic-ui-react';
 
 const activeItem = 'BLOG'
 export default class Blogs extends Component {
@@ -15,6 +15,7 @@ export default class Blogs extends Component {
                         <GridColumn width={12}>
                             <BlogHeader />
                             <BlogCard />
+                            <Pagination defaultActivePage={1} totalPages={10} />
                         </GridColumn>
                         <GridColumn width={4}>
                             <Theme />
@@ -25,6 +26,7 @@ export default class Blogs extends Component {
                     <div style={{ margin: '0 auto', maxWidth: '1000px' }}>
                         <BlogHeader />
                         <BlogCard />
+                        <Pagination defaultActivePage={1} totalPages={10} size='mini' boundaryRange='0' />
                     </div>
                 </Responsive>
             </Layout>
