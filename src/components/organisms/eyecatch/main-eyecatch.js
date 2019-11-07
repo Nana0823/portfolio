33298bcda67ba368a7492d32ae15e581041c724f
Nanako Ohno <nanako@wever.co.jp>
2019-11-07 09:51:45 +0900

    color changed

--- src/components/organisms/eyecatch/main-eyecatch.js
+++ src/components/organisms/eyecatch/main-eyecatch.js
@@ -10,7 +10,7 @@ export default class MainEyecatch extends Component {
             <Responsive>
                 <div class='photo' style={{ position: 'relative', backgroundColor: '#99CCCC' }}>
                     <img alt='ホーム画面' src={Tokyo} style={{ borderRadius: '5px', borderCollapse: 'separate' }} />
-                    <div>
+                    <div style={{ color: 'white' }}>
                         <h2 style={{ fontFamily: 'Bookman Old Style', fontSize: '3vw', margin: '0', paddingBottom: '5%' }}>NANAKO OHNO</h2>
                         <p style={{ fontSize: '1.5vw' }}>Welcome to my portfolio site.</p>
                     </div>
